49d3c3fa32705caa0e265f038fba7ee6203217c4
Dinh Huy <huydinhse@gmail.com>
2022-02-17 15:51:27 +0700

    Finish Lab01_1b

--- Android_TH/app/src/main/java/com/example/myapplication/MainActivity_1a.java
+++ Android_TH/app/src/main/java/com/example/myapplication/MainActivity_1a.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Bai2 extends AppCompatActivity {
+public class MainActivity_1a extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bai2);
+        setContentView(R.layout.activity_main_1a);
     }
 }

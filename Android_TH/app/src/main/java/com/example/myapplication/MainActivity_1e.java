a6d6f3b5f33cefc200f552cc4ca00a0283d5528d
Dinh Huy <huydinhse@gmail.com>
2022-02-18 09:06:36 +0700

    Finish Lab01_1e

---
+++ Android_TH/app/src/main/java/com/example/myapplication/MainActivity_1e.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_1e extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_1e);
+    }
+}
